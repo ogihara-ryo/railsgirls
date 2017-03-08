5ecdb55a7f5c1612ca44a6fa8008c9b90a4f4a52
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-03-08 21:45:41 +0900

    Setup cloudinary

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,20 +2,29 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
+
+  include Cloudinary::CarrierWave
+
+  process convert: 'png'
+  process tags: ['picture']
 
   version :thumb do
     process :resize_to_fill => [50, 50]
   end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def public_id
+    return model.id
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
